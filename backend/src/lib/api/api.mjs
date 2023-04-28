9c3a3d615de7b2cc554edd87b057dae032c5bcd3
agonzalez <agonzalez.develop@gmail.com>
2023-04-28 20:55:00 +0200

    next

---
+++ backend/src/lib/api/api.mjs
@@ -0,0 +1,22 @@
+import { app } from "./RunExpress.mjs"
+import jsonMiddleware from "../middlewares/JsonMiddleware.mjs"
+import { GetUser, GetObject } from "../controllers/GetHandler.mjs"
+import { CreateUser, CreateObjects } from "../controllers/CreateHandler.mjs"
+import { ChangeKarma, UpdateObject } from "../controllers/PutHandler.mjs"
+import { api } from "./path.mjs"
+
+app.get(api.fixedPath, GetUser)
+
+app.get(api.fixedPath+api.userPath, GetUser)
+app.get(api.fixedPath+api.objectPath, GetObject)
+
+app.post(api.fixedPath+api.userPath, jsonMiddleware, CreateUser)
+app.post(api.fixedPath+api.objectPath, jsonMiddleware, CreateObjects)
+
+app.put(api.fixedPath, jsonMiddleware, ChangeKarma)
+app.put(api.fixedPath, jsonMiddleware, UpdateObject)
+
+
+app.listen( process.env.PORT,()=>{
+    console.log("🐧🐧🐧👽👽👽");
+})
---
+++ backend/src/lib/api/path.mjs
@@ -0,0 +1,5 @@
+export const api = {
+    fixedPath: "/api/v1.0/",
+    userPath: "users/",
+    objectPath: "object/"
+}
