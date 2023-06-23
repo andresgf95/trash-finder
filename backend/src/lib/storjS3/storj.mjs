64342d642017ace608aa0778ac33f7f0725d2810
agonzalez <agonzalez.develop@gmail.com>
2023-06-23 20:48:33 +0200

    StorjS3

--- backend/src/lib/storjS3/storj.mjs
+++ backend/src/lib/storjS3/storj.mjs
@@ -6,20 +6,57 @@ import {
     DeleteObjectCommand
 } from "@aws-sdk/client-s3"
 
-const storejEndpoint = "https://gateway.storjshare.io"
+const storejEndpoint = process.env.S3_ENDPOINT
 
 class StoreJ {
 
     constructor(bucket, endpoint = storejEndpoint) {
-        this.bucket = bucket
+        this.bucket = process.env.AWS_BUCKET_NAME
         this.client = new S3Client({
-            region : "eu-south-2",
+            region : process.env.AWS_REGION,
+            credentials: {
+                accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+                secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+            },
             endpoint : endpoint
         })
     }
 
     put(filename, req) {
-        const command = Put
+        const command = PutObjectCommand({
+            Bucket: this.bucket,
+            Key: filename,
+            Body: req,
+            ContentType: req.headers['content-type'],
+            ContentLength: req.headers['content-length']
+        })
+        return this.client.send(command)
+    }
+
+    
+    get(filename) {
+        const command = GetObjectCommand({
+            Bucket: this.bucket,
+            key: filename
+        })
+        return this.client.send(command)
+    }
+
+    list() {
+        const command = ListObjectsCommand({
+            Bucket: this.bucket
+        })
+        return this.client.send(command)
+    }
+
+    Delete(filename) {
+        const command = DeleteObjectCommand({
+            Bucket: this.bucket,
+            Key: filename
+        })
+        return this.client.send(command)
     }
 
 }
+
+export default StoreJ
